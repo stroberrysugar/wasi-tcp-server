51ad4b80441a8c7d26c9594de5a096f4c678b051
Alsan Ali <stroberrysugar@gmail.com>
2022-03-11 20:36:57 +0500

    Removed logging messages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate log;
-
 use wasi::{
     fd_close, fd_read, poll_oneoff, sock_accept, Event, Iovec, Subscription,
     SubscriptionFdReadwrite, SubscriptionU, SubscriptionUU, ERRNO_AGAIN, FDFLAGS_NONBLOCK,
@@ -20,22 +17,12 @@ fn main() {
     let mut events = vec![unsafe { std::mem::zeroed::<Event>() }; 1];
 
     loop {
-        debug!("[0.0] Looping");
-
         let num = unsafe {
             poll_oneoff(subscription.as_ptr(), events.as_mut_ptr(), events.len()).unwrap()
         };
 
-        debug!("[1.0] Polled ({} events)", num);
-
         for i in 0..num {
             let event = &events[i];
-
-            debug!(
-                "[2.0] Handling event `{}` of type `{}`",
-                event.userdata,
-                event.type_.raw()
-            );
 
             match event.userdata {
                 1 => accept_connection(&mut subscription),
@@ -48,16 +35,12 @@ fn main() {
 }
 
 fn accept_connection(subscription: &mut Vec<Subscription>) {
-    debug!("[3.0] Attempting to accept connection");
-
     let file_descriptor = unsafe {
         match sock_accept(3, FDFLAGS_NONBLOCK) {
             Ok(n) => n,
             Err(_) => return,
         }
     };
-
-    debug!("[3.1] Accepted connection. FD = {}", file_descriptor);
 
     subscription.push(Subscription {
         userdata: file_descriptor as u64,
@@ -72,7 +55,6 @@ fn accept_connection(subscription: &mut Vec<Subscription>) {
 
 fn handle_read(event: &Event, subscription: &mut Vec<Subscription>) {
     if event.fd_readwrite.nbytes == 0 {
-        debug!("[4.2] Removing socket {}", event.userdata);
         subscription.retain(|x| x.userdata != event.userdata);
 
         unsafe {
@@ -88,21 +70,11 @@ fn handle_read(event: &Event, subscription: &mut Vec<Subscription>) {
         buf_len: buf.len(),
     }];
 
-    debug!(
-        "[4.0] Attempting to read {} bytes from socket {}",
-        event.fd_readwrite.nbytes, event.userdata
-    );
-
     let num = unsafe {
         match fd_read(event.userdata as u32, &iovec) {
             Ok(n) => n,
             Err(e) => {
                 if e != ERRNO_AGAIN {
-                    debug!(
-                        "[4.2] Removing socket {} due to error `{:?}`",
-                        event.userdata, e
-                    );
-
                     subscription.retain(|x| x.userdata != event.userdata);
 
                     fd_close(event.userdata as u32).unwrap();
